b6fd0b6914e1de07754849d3a60178881f619a51
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-01-28 06:53:34 +0100

    chore: Set up project structure.

---
+++ dd_wi_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DDWIMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dd_wi_main'
---
+++ reports/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DDWIReportsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dd_wi_reports'
