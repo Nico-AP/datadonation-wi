05bd9ae150b913e90db1caed5a4b3514cfa8ad74
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-01-30 12:31:33 +0100

    chore: Add DEBUG settings to url config.

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -10,3 +12,7 @@ urlpatterns = [
     path('ddm/logout/', auth_views.LogoutView.as_view(), name='ddm_logout'),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+                   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
