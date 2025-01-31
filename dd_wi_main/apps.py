39eb74ee47b5bea551f7235dc304d23feeba8c41
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-01-31 11:53:34 +0100

    chore: Add verbose app names.

--- dd_wi_main/apps.py
+++ dd_wi_main/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DDWIMainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dd_wi_main'
+    verbose_name = 'Data Donation WI'
--- reports/apps.py
+++ reports/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DDWIReportsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reports'
+    verbose_name = 'Data Donation Reports'
