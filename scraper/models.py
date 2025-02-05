dec68628554d0b17d08c35c1e1b4b9b5cd3e5b20
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-05 18:28:19 +0100

    fix: Allow TikTokVideo.music_id to be null.

--- scraper/models.py
+++ scraper/models.py
@@ -19,15 +19,24 @@ class TikTokVideo(models.Model):
     video_id = models.BigIntegerField(unique=True)
     video_description = models.TextField(null=True, blank=True)
     create_time = models.DateTimeField()
-    username = models.ForeignKey(TikTokUser, on_delete=models.SET_NULL, null=True)
+    username = models.ForeignKey(
+        TikTokUser,
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     comment_count = models.IntegerField()
     like_count = models.IntegerField()
     share_count = models.IntegerField()
     view_count = models.IntegerField()
 
-    hashtags = models.ManyToManyField(Hashtag, blank=True, related_name='tt_videos')
-    music_id = models.BigIntegerField()
+    hashtags = models.ManyToManyField(
+        Hashtag,
+        blank=True,
+        null=True,
+        related_name='tt_videos'
+    )
+    music_id = models.BigIntegerField(null=True)
     region_code = models.CharField(max_length=255)
 
     def __str__(self):
