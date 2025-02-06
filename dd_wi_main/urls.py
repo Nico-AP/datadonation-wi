cd4cd750d0703afe1c7a73c6613f75da8c67dae6
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-06 18:22:26 +0100

    fix: Disable wip page per default.

--- dd_wi_main/urls.py
+++ dd_wi_main/urls.py
@@ -3,5 +3,5 @@ from .views import LandingView, WipView
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
-    path('wip', WipView.as_view(), name='wip'),
+    # path('wip', WipView.as_view(), name='wip'),
 ]
