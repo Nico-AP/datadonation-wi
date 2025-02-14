09380e985558b1b18abea1729be7daec14901273
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-14 15:49:13 +0100

    chore: Update celery settings.

--- config/celery.py
+++ config/celery.py
@@ -6,12 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Set default settings for Celery
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
-app = Celery("config")
+app_name = os.environ.get('CELERY_APP_NAME', 'config')
+app = Celery(app_name)
 
 # Load task modules from all registered Django apps
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Auto-discover tasks from installed apps
 app.autodiscover_tasks()
