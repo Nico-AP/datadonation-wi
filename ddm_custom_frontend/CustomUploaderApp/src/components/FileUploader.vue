fe1c90d38552f5415de64793b12fdf3386b47282
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-28 13:47:54 +0100

    chore: Update custom uploader to accept old and new file strucutres of both json and txt uploads.

--- ddm_custom_frontend/CustomUploaderApp/src/components/FileUploader.vue
+++ ddm_custom_frontend/CustomUploaderApp/src/components/FileUploader.vue
@@ -2,7 +2,6 @@
 
 <template>
 
-  <div>dldjlkdsjflkjdsflkjdslkfjdj</div>
 
   <div class="mb-5">
     <div class="float-left pt-2 ps-2 rounded-top">
@@ -494,17 +493,16 @@ export default {
 
                     /* try to find corresponding text file */
                     let extractionRoot = blueprint.json_extraction_root;
-                    console.log(extractionRoot)
                     const fileLookup = {
-                      "Your Activity.Video Browsing History.VideoList": "Watch History.txt",
-                      "Your Activity.Like List.ItemFavoriteList": "Like List.txt",
-                      "Your Activity.Search History.SearchList": "Searches.txt",
-                      "Your Activity.Share History.ShareHistoryList": "Share History.txt",
-                      "Video.Videos.VideoList": "Post.txt",
-                      "Comment.Comments.CommentsList": "Comments.txt",
-                      "Your Activity.Follower List.FansList": "Follower.txt",
-                      "Your Activity.Following List.Following": "Following.txt",
-                      "App Settings.Block.BlockList": "Block List.txt"
+                      "Your Activity.Video Browsing History.VideoList": /(?:^|\/)(Browsing History\.txt|Watch History\.txt)$/,
+                      "Your Activity.Like List.ItemFavoriteList": "Like List\\.txt",
+                      "Your Activity.Search History.SearchList": "Searches\\.txt",
+                      "Your Activity.Share History.ShareHistoryList": "Share History\\.txt",
+                      "Video.Videos.VideoList": /(?:^|\/)(Post\.txt|Posts\.txt)$/,
+                      "Comment.Comments.CommentsList": "Comments\\.txt",
+                      "Your Activity.Follower List.FansList": "Follower\\.txt",
+                      "Your Activity.Following List.Following": "Following\\.txt",
+                      "App Settings.Block.BlockList": "Block List\\.txt"
                     };
                     let txtFile = fileLookup[extractionRoot] || null;
 
@@ -688,7 +686,16 @@ export default {
         if(fileContent) {
           if (blueprint.json_extraction_root !== '') {
             try {
-              fileContent = this.getNestedJsonEntry(fileContent, blueprint.json_extraction_root);
+              let extractedContent = this.getNestedJsonEntry(fileContent, blueprint.json_extraction_root);
+
+              if (!extractedContent) {
+                if (blueprint.json_extraction_root.startsWith("Your ")) {
+                  let altExtractionRoot = blueprint.json_extraction_root.slice(5);
+                  extractedContent = this.getNestedJsonEntry(fileContent, altExtractionRoot);
+                }
+              }
+              fileContent = extractedContent;
+
             } catch (e) {
               uploader.postError(4207, uploader.$t('error-no-data-in-file'), blueprint.id);
               uploader.recordError(uploader.$t('error-no-data-in-file'), blueprintID);
@@ -1033,8 +1040,7 @@ export default {
       let nSuccess = 0;
       let nFailed = 0;
       let nBlueprints = Object.keys(this.blueprintData).length;
-      console.log(this.generalErrors)
-      console.log(this.generalErrors.length)
+
       for (let bp in this.blueprintData){
         if (this.generalErrors.length !== 0) {
           this.blueprintData[bp].status = 'failed';
@@ -1080,7 +1086,7 @@ export default {
           this.$refs.ulInfoModal.style.display = 'block';
           this.$refs.modalBackdrop.style.display = 'block';
 
-        } else if ((nSuccess + nNothingExtracted) === nBlueprints) {
+        } else if (((nSuccess + nNothingExtracted) === nBlueprints) || (nSuccess > 0 && nSuccess < nBlueprints)) {
           this.uploadStatus = 'success';
           modalIcon.className = 'bi bi-file-check text-success';
           this.ulModalInfoTitle = this.$t('ul-success-modal-title');
