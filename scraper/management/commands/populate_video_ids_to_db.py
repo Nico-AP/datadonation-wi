68d1052fc3db119d0637ebd6797f242282a8dd37
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-03-07 15:33:15 +0100

    chore: Add progress bar to populate_video_ids_to_db command.

--- scraper/management/commands/populate_video_ids_to_db.py
+++ scraper/management/commands/populate_video_ids_to_db.py
@@ -1,8 +1,20 @@
+import sys
+
 from django.core.management.base import BaseCommand
 from ddm.datadonation.models import DataDonation, DonationBlueprint
-
+from rich.console import Console
 from scraper.models import TikTokVideo_B
+from tqdm import tqdm
 
+console = Console()
+
+
+def print_to_console(msg):
+    sys.stdout.write('\033[F')  # Move cursor up to overwrite the last task message
+    sys.stdout.write('\033[K')  # Clear the line
+    console.print(msg, end='')
+    sys.stdout.write('\033[E')  # Moves cursor down one line
+    sys.stdout.flush()
 
 class Command(BaseCommand):
     help = 'Extract video IDs from donations and add to database.'
@@ -13,11 +25,21 @@ class Command(BaseCommand):
             type=int,
             help='ID of the watched videos blueprint.'
         )
+        parser.add_argument(
+            '--max_donations',
+            type=int,
+            help='Number of donations to process.'
+        )
 
     def handle(self, *args, **options):
+        console.print('[white]Start populate_video_ids_to_db.[/]')
+
         # Get watched videos blueprint
         bp_id_watched_videos = options.get('bp_id')
         bp = DonationBlueprint.objects.get(pk=bp_id_watched_videos)
+
+        # Get n donation to process.
+        max_donations = options.get('max_donations')
 
         # Get project.
         self.project = bp.project
@@ -28,18 +50,23 @@ class Command(BaseCommand):
             consent=True,
             status='success'
         )
-        i = 0
+
+        if max_donations:
+            donations = donations[:max_donations]
+
+        pbar = tqdm(total=len(donations), dynamic_ncols=True, position=0, leave=True, colour="magenta")
         for donation in donations:
             self.extract_video_ids(donation)
-            i += 1
-            print(f'{i}/{donations.count()} donations processed.')
+            pbar.update(1)
+        pbar.update(1)
 
-        print('Done')
+        console.print('[bold green]✅ All entries added to db.[/]')
         return
 
     def extract_video_ids(self, donation):
         data = donation.get_decrypted_data(
             self.project.secret, self.project.get_salt())
+        participant = donation.participant.external_id
 
         video_ids = [item["Link"].rstrip("/").split("/")[-1] for item in data]
         video_ids_clean = []
@@ -60,4 +87,5 @@ class Command(BaseCommand):
         # Insert data in batches
         BATCH_SIZE = 10000
         for i in range(0, len(new_videos), BATCH_SIZE):
+            print_to_console(f'[cyan]Processing {participant}: [yellow] Adding video ids {i}:{i + BATCH_SIZE}.')
             TikTokVideo_B.objects.bulk_create(new_videos[i:i + BATCH_SIZE], ignore_conflicts=True)
