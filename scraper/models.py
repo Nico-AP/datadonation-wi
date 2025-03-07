496b839f20723d6323d4ea8043a224e725a2877a
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-03-07 15:11:25 +0100

    fix: Move from BigIntField to DecimalField on TikToVideo_B and TikTokUser_B models. Additionally, add blank=True on fields where null=True.

--- scraper/models.py
+++ scraper/models.py
@@ -21,23 +21,24 @@ class TikTokUser_B(models.Model):
     # name = username in TikTokUser
     username = models.CharField(max_length=255)
     ###### ADDED FIELDS ######
-    author_id = models.BigIntegerField(null=True, unique=True)
+    author_id = models.DecimalField(
+        max_digits=100, decimal_places=0, unique=True, null=True, blank=True)
     ## in TikTok_Content_Scraper scheme "name"
-    nick_name = models.CharField(max_length=255, null=True)
-    signature = models.TextField(null=True)
-    create_time = models.IntegerField(null=True)
+    nick_name = models.CharField(max_length=255, null=True, blank=True)
+    signature = models.TextField(null=True, blank=True)
+    create_time = models.IntegerField(null=True, blank=True)
     verified = models.BooleanField(default=False)
     ftc = models.BooleanField(default=False)
-    relation = models.IntegerField(null=True)
+    relation = models.IntegerField(null=True, blank=True)
     open_favorite = models.BooleanField(default=False)
-    comment_setting = models.IntegerField(null=True)
-    duet_setting = models.SmallIntegerField(null=True)
-    stitch_setting = models.SmallIntegerField(null=True)
+    comment_setting = models.IntegerField(null=True, blank=True)
+    duet_setting = models.SmallIntegerField(null=True, blank=True)
+    stitch_setting = models.SmallIntegerField(null=True, blank=True)
     private_account = models.BooleanField(default=False)
     secret = models.BooleanField(default=False)
     is_ad_virtual = models.BooleanField(default=False)
-    download_setting = models.SmallIntegerField(null=True)
-    recommend_reason = models.CharField(max_length=255, null=True)
+    download_setting = models.SmallIntegerField(null=True, blank=True)
+    recommend_reason = models.CharField(max_length=255, null=True, blank=True)
     suggest_account_bind = models.BooleanField(default=False)
 
     class Meta:
@@ -81,27 +82,27 @@ class TikTokVideo(models.Model):
 
 
 class TikTokVideo_B(models.Model):
-    video_id = models.BigIntegerField(unique=True)
+    video_id = models.DecimalField(max_digits=100, decimal_places=0, unique=True)
     video_description = models.TextField(null=True, blank=True)
-    create_time = models.DateTimeField(null=True)
+    create_time = models.DateTimeField(null=True, blank=True)
     author_id = models.ForeignKey(
         TikTokUser_B,
         on_delete=models.SET_NULL,
-        null=True
+        null=True, blank=True
     )
 
-    comment_count = models.IntegerField(null=True)
-    like_count = models.IntegerField(null=True)
-    share_count = models.IntegerField(null=True)
-    view_count = models.IntegerField(null=True)
+    comment_count = models.IntegerField(null=True, blank=True)
+    like_count = models.IntegerField(null=True, blank=True)
+    share_count = models.IntegerField(null=True, blank=True)
+    view_count = models.IntegerField(null=True, blank=True)
 
     hashtags = models.ManyToManyField(
         Hashtag,
         blank=True,
         related_name='tt_videos_b'
     )
-    music_id = models.BigIntegerField(null=True)
-    region_code = models.CharField(max_length=255, null=True)
+    music_id = models.BigIntegerField(null=True, blank=True)
+    region_code = models.CharField(max_length=255, null=True, blank=True)
 
     ##### NEW METADATA FIELDS #####
     # new fields for table B
@@ -111,50 +112,50 @@ class TikTokVideo_B(models.Model):
         related_name='mentions_b'
     )
 
-    schedule_time = models.IntegerField(null=True)
+    schedule_time = models.IntegerField(null=True, blank=True)
     is_ad = models.BooleanField(default=False)
-    suggested_words = models.CharField(max_length=255, null=True)
-    diggcount = models.IntegerField(null=True)
-    collectcount = models.IntegerField(null=True)
-    repostcount = models.IntegerField(null=True)
-    poi_name = models.CharField(max_length=255, null=True)
-    poi_address = models.CharField(max_length=255, null=True)
-    poi_city = models.CharField(max_length=255, null=True)
-    warn_info = models.JSONField(null=True)
+    suggested_words = models.CharField(max_length=255, null=True, blank=True)
+    diggcount = models.IntegerField(null=True, blank=True)
+    collectcount = models.IntegerField(null=True, blank=True)
+    repostcount = models.IntegerField(null=True, blank=True)
+    poi_name = models.CharField(max_length=255, null=True, blank=True)
+    poi_address = models.CharField(max_length=255, null=True, blank=True)
+    poi_city = models.CharField(max_length=255, null=True, blank=True)
+    warn_info = models.JSONField(null=True, blank=True)
     original_item = models.BooleanField(default=False)
     offical_item = models.BooleanField(default=False)
     secret = models.BooleanField(default=False)
     for_friend = models.BooleanField(default=False)
     digged = models.BooleanField(default=False)
-    item_comment_status = models.SmallIntegerField(null=True)
-    take_down = models.IntegerField(null=True)
-    effect_stickers = models.JSONField(null=True)  # Array of strings
+    item_comment_status = models.SmallIntegerField(null=True, blank=True)
+    take_down = models.IntegerField(null=True, blank=True)
+    effect_stickers = models.JSONField(null=True, blank=True)  # Array of strings
     private_item = models.BooleanField(default=False)
     duet_enabled = models.BooleanField(default=False)
     stitch_enabled = models.BooleanField(default=False)
-    stickers_on_item = models.JSONField(null=True)  # Array of strings
+    stickers_on_item = models.JSONField(null=True, blank=True)  # Array of strings
     share_enabled = models.BooleanField(default=False)
-    comments = models.JSONField(null=True)  # Array of comment objects
-    duet_display = models.IntegerField(null=True)
-    stitch_display = models.IntegerField(null=True)
+    comments = models.JSONField(null=True, blank=True)  # Array of comment objects
+    duet_display = models.IntegerField(null=True, blank=True)
+    stitch_display = models.IntegerField(null=True, blank=True)
     index_enabled = models.BooleanField(default=False)
-    diversification_labels = models.JSONField(null=True)  # Array of strings
-    diversification_id = models.BigIntegerField(null=True)
-    channel_tags = models.JSONField(null=True)  # Array of strings
-    keyword_tags = models.JSONField(null=True)  # Array of strings
+    diversification_labels = models.JSONField(null=True, blank=True)  # Array of strings
+    diversification_id = models.BigIntegerField(null=True, blank=True)
+    channel_tags = models.JSONField(null=True, blank=True)  # Array of strings
+    keyword_tags = models.JSONField(null=True, blank=True)  # Array of strings
     is_ai_gc = models.BooleanField(default=False)
-    ai_gc_description = models.TextField(null=True)
+    ai_gc_description = models.TextField(null=True, blank=True)
 
-    filepath = models.TextField(null=True)
-    duration = models.IntegerField(null=True)
-    height = models.IntegerField(null=True)
-    width = models.IntegerField(null=True)
-    ratio = models.IntegerField(null=True)
-    volume_loudness = models.FloatField(null=True)
-    volume_peak = models.FloatField(null=True)
-    has_original_audio = models.BooleanField(null=True, default=None)
-    enable_audio_caption = models.BooleanField(null=True, default=None)
-    no_caption_reason = models.SmallIntegerField(null=True)
+    filepath = models.TextField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
+    width = models.IntegerField(null=True, blank=True)
+    ratio = models.IntegerField(null=True, blank=True)
+    volume_loudness = models.FloatField(null=True, blank=True)
+    volume_peak = models.FloatField(null=True, blank=True)
+    has_original_audio = models.BooleanField(null=True, default=None, blank=True)
+    enable_audio_caption = models.BooleanField(null=True, default=None, blank=True)
+    no_caption_reason = models.SmallIntegerField(null=True, blank=True)
 
     scrape_date = models.DateTimeField(
         null=True,
